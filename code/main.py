5109c592202ab2aebf16c3f9aef97c12ff46979c
Rick Shangle <rshangle@gmail.com>
2025-09-18 14:14:08 -0500

    working on CORS issue

--- code/main.py
+++ code/main.py
@@ -8,3 +8,14 @@ app = FastAPI(
 )
 
 app.include_router(upload_router)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for local development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
